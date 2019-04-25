ef50429db22351e9b37d34acb7b83fc75caafda8
Mikkel Svartveit <mikkels@ostfoldfk.no>
2019-04-25 22:00:25 +0200

    jobbet videre med prosjekt

---
+++ restaurant/admin/endrematrett.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Rediger matrett</title>
+    <link href="admin.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+</head>
+
+<body>
+    <h2>Rediger matrett</h2>
+
+    <?php
+    $servernavn = "localhost";
+    $brukernavn = "root";
+    $passord = "";
+    $dbnavn = "italia";
+
+    $tilkobling = mysqli_connect($servernavn, $brukernavn, $passord, $dbnavn);
+
+    if($tilkobling->connect_error) {
+        die("Noe gikk galt: " . $tilkobling->connect_error);
+    }
+
+    $tilkobling->set_charset("utf8");
+    
+    $id = $_GET["id"];
+    
+    if(isset($_POST["submit"])) {
+        $navn = $_POST["navn"];
+        $type = $_POST["type"];
+        $pris = $_POST["pris"];
+        $informasjon = $_POST["informasjon"];
+
+        $sql = "UPDATE matrett SET navn='$navn', type='$type', pris=$pris, informasjon='$informasjon' WHERE idmatrett=$id";
+
+        if($tilkobling->query($sql)) {
+            echo "<script>location.href = 'meny.php'</script>";
+        }
+    }
+    
+    $data = $tilkobling->query("SELECT * FROM matrett WHERE idmatrett = $id");
+    $row = mysqli_fetch_array($data);
+    ?>
+    
+    <form action="" method="post">
+    <table>
+        <tr>
+            <th>Bilde</th>
+            <th>Navn</th>
+            <th>Type</th>
+            <th>Pris</th>
+            <th>Beskrivelse</th>
+        </tr>
+        <tr>
+            <td><img src="upload/<?php echo $row["bilde"]?>"></td>
+            <td><input type="text" name="navn" value="<?php echo $row['navn']?>"></td>
+            <td>
+                <input type="radio" name="type" value="f" required>Forrett<br>
+                <input type="radio" name="type" value="h" required>Hovedrett<br>
+                <input type="radio" name="type" value="d" required>Dessert
+            </td>
+            <td><input type="number" name="pris" style="width: 80px" value="<?php echo $row['pris']?>"> kr</td>
+            <td><textarea name="informasjon" cols="30" rows="6"><?php echo $row['informasjon']?></textarea></td>
+        </tr>
+    </table>
+    <br>
+    <input type="submit" name="submit" value="Lagre">
+    </form>
+    <button onclick="location.href = 'meny.php'">Avbryt</button>
+    
+    <?php
+    $value = $row['type'];
+    echo "<script>document.querySelector('input[value=$value]').checked = true;</script>"; 
+    ?>
+</body>
+
+</html>
--- restaurant/admin/meny.php
+++ restaurant/admin/meny.php
@@ -1,29 +1,15 @@
 <!doctype html>
 <html>
-    
+
 <head>
     <meta charset="utf-8">
     <title>Administrer meny</title>
+    <link href="admin.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
 </head>
+
 <body>
     <h1>Administrer meny</h1>
-    
-    <h2>Legg til matrett</h2>
-    <form action="" method="post" enctype="multipart/form-data">        
-        Navn på matrett: <input type="text" name="navn"><br><br>
-        
-        Type: <input type="radio" name="type" value="f">Forrett
-        <input type="radio" name="type" value="h">Hovedrett
-        <input type="radio" name="type" value="d">Dessert<br><br>
-        
-        Pris: <input type="number" name="navn"> kr<br><br>
-        
-        Bilde: <input type="file" name="fil" id="fil"><br><br>            
-        
-        Beskrivelse: <br><textarea name="beskrivelse" id="beskrivelse" cols="40" rows="6"></textarea><br><br>
-        
-        <input type="submit" name="submit" id="submit" value="Legg til matrett">
-    </form>
     
     <?php
     $servernavn = "localhost";
@@ -39,8 +25,121 @@
 
     $tilkobling->set_charset("utf8");
     
-    
+    if(isset($_GET["slett"])) {
+        $slettid = $_GET["slett"];
+        
+//        $sql = $tilkobling->query("SELECT bilde FROM matrett WHERE idmatrett=$slettid");
+//        if($row = mysqli_fetch_array($sql)) {
+//            if(file_exists("upload/" . $row["bilde"])) {
+//                unlink("upload/" . $row["bilde"]);
+//            }
+//        }
+        
+        $tilkobling->query("DELETE FROM matrett WHERE idmatrett=$slettid");
+    }
     ?>
-</body>
     
+    <div>
+        <div>
+            <h2>Legg til matrett</h2>
+            <form action="" method="post" enctype="multipart/form-data">
+                Navn på matrett: <input type="text" name="navn" required><br><br>
+
+                Type: <input type="radio" name="type" value="f" required>Forrett
+                <input type="radio" name="type" value="h" required>Hovedrett
+                <input type="radio" name="type" value="d" required>Dessert<br><br>
+
+                Pris: <input type="number" name="pris" style="width: 80px" required> kr<br><br>
+
+                Bilde: <input type="file" name="file" id="file"><br><br>
+
+                Beskrivelse: <br><textarea name="beskrivelse" cols="40" rows="6"></textarea><br><br>
+
+                <input type="submit" name="submit" value="Legg til matrett">
+            </form>
+
+            <br>
+
+            <?php
+            if(isset($_POST["submit"])) {
+                //Start opplasting
+                $allowedExts = array("gif", "jpeg", "jpg", "png");
+                $temp = explode(".", $_FILES["file"]["name"]);
+                $extension = end($temp);
+
+                //Her kan du slette eller legge til de filformatene du ønsker skal godtas.
+                if ((($_FILES["file"]["type"] == "image/gif")
+                || ($_FILES["file"]["type"] == "image/jpeg")
+                || ($_FILES["file"]["type"] == "image/jpg")
+                || ($_FILES["file"]["type"] == "image/png"))
+                && ($_FILES["file"]["size"] < 10000000) //Her kan du endre max filstørrelse. 
+                && in_array($extension, $allowedExts)) { 
+                    if ($_FILES["file"]["error"]> 0) { //Feilmelding hvis filformat/størrelse er feil.
+                        echo "Return
+
+                        Code: " . $_FILES["file"]["error"] . "<br>";
+                    }
+
+                    if (file_exists("upload/" . $_FILES["file"]["name"])) {
+                        echo $_FILES["file"]["name"] . " finnes allerede i mappen. "; //Melding: Filen eksisterer.
+                    }
+                    else {
+                        move_uploaded_file($_FILES["file"]["tmp_name"], //Filen flyttes fra variabelen $_FILES til katalogen "upload".
+                        "upload/" . $_FILES["file"]["name"]);
+                    }
+                }
+
+                $navn = $_POST["navn"];
+                $type = $_POST["type"];
+                $pris = $_POST["pris"];
+                $beskrivelse = $_POST["beskrivelse"];
+                $filnavn = $_FILES["file"]["name"];
+
+                $sql = "INSERT INTO matrett(navn, type, pris, informasjon, bilde)
+                VALUES('$navn', '$type', $pris, '$beskrivelse', '$filnavn')";
+
+                if($tilkobling->query($sql)) {
+                    echo "<b>Matrett lagt til</b>";
+                }
+                else {
+                    echo "<b>Noe gikk galt: $sql($tilkobling->error)</b>";
+                }
+            }
+            ?>
+            
+            <br>
+        </div>
+
+        <div>
+            <h2>Gjeldende meny</h2>
+            <table>
+                <tr>
+                    <th>Bilde</th>
+                    <th>Navn på rett</th>
+                    <th>Type</th>
+                    <th>Pris</th>
+                    <th>Beskrivelse</th>
+                    <th></th>
+                </tr>
+                <?php 
+                $data = $tilkobling->query("SELECT * FROM matrett ORDER BY type, navn");
+                while($row = mysqli_fetch_array($data)) { ?>
+                <tr>
+                    <td><img src="upload/<?php echo $row["bilde"]?>"></td>
+                    <td><?php echo $row["navn"]?></td>
+                    <td><?php echo $row["type"]?></td>
+                    <td><?php echo $row["pris"]?> kr</td>
+                    <td><?php echo $row["informasjon"]?></td>
+                    <td>
+                        <button onclick="location.href='endrematrett.php?id=<?php echo $row["idmatrett"]?>'">Endre</button>
+                        <br><br>
+                        <button onclick="location.href='?slett=<?php echo $row["idmatrett"]?>'">Fjern</button>
+                    </td>
+                </tr>
+                <?php } ?>
+            </table>
+        </div>
+    </div>
+</body>
+
 </html>
